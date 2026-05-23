2f40bdab8dd1137848ee67fea87f2e2ddc2b9411
Danilo Varon <danilovarons@gmail.com>
2026-05-23 16:29:26 -0500

    Se realizan las configuraciones iniciales del proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    // Configuración de Ionic para Angular
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    // Configuración de rutas con precarga
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
